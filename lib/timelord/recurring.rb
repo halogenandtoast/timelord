8912783c3cd564401d0dd9ae6d5c2bcc1f0030ce
Matthew Mongeau <matt@toastyapps.com>
2011-02-17 20:43:49 -0500

    More wip

--- lib/timelord/recurring.rb
+++ lib/timelord/recurring.rb
@@ -1,5 +1,33 @@
 module Timelord
   class Recurring
+    FREQUENCY = [:secondly, :minutely, :hourly, :daily, :weekly, :monthy, :yearly]
 
+    def initialize(start, options = {})
+
+      @start = start
+      @current = start
+
+      @freq = options[:freq]
+      @until = nil
+      @count = nil
+      @interval = nil
+      @bysecond = nil #list
+      @byminute = nil #list
+      @byhour = nil #list
+      @byday = nil
+      @bymonthday = nil
+      @byyearday = nil
+      @byweekno = nil
+      @bymonth = nil
+      @bysetpos = nil
+      @wkst = "SU"
+    end
+
+    def next
+      if @freq == :daily
+        @current = Time.local_time(@current.year, @current.month, @current.day) + 86400
+      end
+      @current
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'timelord'
 require 'rubygems'
+require 'rspec'
 require 'bundler/setup'
 require 'timecop'
